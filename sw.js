e9f6b52b0250b43adc0a64eaafd75e88f9db6386
Raymond <yunjie1.ou@midea.com>
2019-07-11 15:08:49 +0800

    ssss

--- sw.js
+++ sw.js
@@ -19,7 +19,6 @@ const PRECACHE_LIST = [
   "./js/bootstrap.min.js",
   "./js/hux-blog.min.js",
   "./js/snackbar.js",
-  "./img/icon_wechat.png",
   "./img/avatar-raymond.jpg",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
